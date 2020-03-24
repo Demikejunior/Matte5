3123f0b9308026b74cc37bd10c46b62534248504
Demikejunior <42673940+Demikejunior@users.noreply.github.com>
2020-03-24 14:57:56 +0100

    Förtydlingar

--- src/talteori/uppg1.py
+++ src/talteori/uppg1.py
@@ -1,5 +1,5 @@
 x = 215
 y = 159
 
-print(x % 7)
-print(y % 7)
+print(x, "mod 7 =", x % 7)
+print(y, "mod 7 =", y % 7)
--- src/talteori/uppg2.py
+++ src/talteori/uppg2.py
@@ -1,5 +1,4 @@
 x = 43
 y = (x - 1) % 5 + 1
 
-print(x % 5)
-print(y % 5)
+print(y)
