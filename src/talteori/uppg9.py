a199da3722f8d64aa31634a1d9d56ef74496821a
SpikeYr <emil.f.anderson@gmail.com>
2020-03-26 15:58:26 +0100

    uppg 9 förtydligande

--- src/talteori/uppg9.py
+++ src/talteori/uppg9.py
@@ -5,4 +5,6 @@ def rec2(n):
     return 2 * n + 3
 
 for n in range(0, 10):
-    print(rec1(n), rec2(rec1(n - 1)))
+    print("Formel ett av", rec1(n - 1), "=", rec1(n))
+    print("Formel två av", rec1(n-1), "=", rec2(rec1(n - 1)))
+    print()
