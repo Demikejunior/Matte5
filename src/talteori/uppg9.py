a03ea4bb6066209dcccf9fa3198d7e69920234be
SpikeYr <emil.f.anderson@gmail.com>
2020-04-02 15:19:37 +0200

    Analytisk uppg 9

--- src/talteori/uppg9.py
+++ src/talteori/uppg9.py
@@ -1,10 +1,16 @@
-def rec1(n):
-    return 5 * (2 ** n)  - 3
+def a(n):
+	return 5 * (2 ** n)  - 3
 
-def rec2(n):
-    return 2 * n + 3
-
-for n in range(0, 10):
-    print("Formel ett av", rec1(n - 1), "=", rec1(n))
-    print("Formel två av", rec1(n-1), "=", rec2(rec1(n - 1)))
-    print()
+print("Rekursiv formel def: a(0) = 2,")
+print("a(n + 1) = 2a(n) + 3")
+print()
+print("Sluten formel def: a(n) = 5 * 2 ^ (n) - 3")
+print("a(0) =", a(0))
+print("a(n + 1) = 5 * 2 ^ (n + 1) - 3")
+print()
+print("Bevis att Rekursiv == Sluten:")
+print("Rekursiv a(n+1) av Sluten a(n):")
+print("2 * (5 * 2 ^ (n) - 3) + 3 =")
+print("= 5 * 2 ^ (n + 1) - 6 + 3 =")
+print("= 5 * 2 ^ (n + 1) - 6 = Sluten a(n+1)")
+print("Q.E.D")
