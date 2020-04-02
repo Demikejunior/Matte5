0418f92be9a3f46a819c5d34739c7e1d10812192
SpikeYr <emil.f.anderson@gmail.com>
2020-04-02 14:04:09 +0200

    uppg 12 & 13

---
+++ src/talteori/aritmetiskTalföljd.py
@@ -0,0 +1,20 @@
+def prog(n, am, m, d):
+    return am + (n - m) * d
+
+#Uppgift 12
+# a)
+
+print("a100 =", prog(100, 8, 2, 4))
+
+# b)
+
+print("an = 4n")
+
+
+#13
+# a)
+
+print(prog(5, 10, 4, (40 - 10) / 2))
+
+# b)
+
