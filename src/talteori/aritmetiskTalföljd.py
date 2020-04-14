8d5c2294c3887110663503e2bfac617d6b9420d6
SpikeYr <emil.f.anderson@gmail.com>
2020-04-14 15:15:52 +0200

    uppg 12 & 13 delupgiftsseperation

--- src/talteori/aritmetiskTalföljd.py
+++ src/talteori/aritmetiskTalföljd.py
@@ -3,19 +3,21 @@ def prog(n, am, m, d):
 
 #Uppgift 12
 # a)
-
+print("a)")
 print("a100 =", prog(100, 8, 2, 4))
+print()
 
 # b)
-
+print("b)")
 print("an = 4n")
-
+print()
 
 #13
 # a)
-
+print("a)")
 print(prog(5, 10, 4, (40 - 10) / 2))
+print()
 
 # b)
-
+print("b)")
 print(prog(16, 10, 4, 15))
